2ba2acef2b5cefe928dbd6cf8124896e0195b7ff
CBedoy <carlos.bedoy@gmail.com>
2014-03-16 16:14:45 -0600

    Add GrandSlamModel

---
+++ cb/models/GrandSlam.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cb.models;
+
+/**
+ *
+ * @author Carlos
+ */
+public class GrandSlam {
+    private int id;
+    private String name;
+    private String country;
+    private String place;
+    private String date;
+    
+    
+}
