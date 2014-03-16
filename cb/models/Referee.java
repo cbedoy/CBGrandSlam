345412d64b15f15af648bcbabf5330e56ad8a439
CBedoy <carlos.bedoy@gmail.com>
2014-03-16 16:19:02 -0600

    Add Referee class

---
+++ cb/models/Referee.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cb.models;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Referee {
+    private int id;
+    private String name;
+    private String nacionality;
+    private String time;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNacionality() {
+        return nacionality;
+    }
+
+    public void setNacionality(String nacionality) {
+        this.nacionality = nacionality;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
