fe910469465c5e6325e41202eb4e71c900132c0c
CBedoy <carlos.bedoy@gmail.com>
2014-03-16 16:33:49 -0600

    Implement fuctionality of class

--- cb/bisness/ConectionDB.java
+++ cb/bisness/ConectionDB.java
@@ -6,10 +6,70 @@
 
 package cb.bisness;
 
-/**
- *
- * @author Carlos
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+
+/*
+    CLASE CONEXION
+    Esta clase sirve para realizar la conexion a la base de datos
+    con la que trabaja el sistema
+
+*/
+
 public class ConectionDB {
+    private Connection ct;
+    private Statement st;  
+    private final static String driver = "com.mysql.jdbc.Driver";
+    private final static String db = "jdbc:mysql://localhost:3306/codice";
+    private String message;
+    private boolean conectado;
+    
+    
+    public ConectionDB(){
+        try{
+            Class.forName(driver).newInstance();
+            ct = DriverManager.getConnection(db, "root", "");
+            st = ct.createStatement();
+            conectado = true;
+            
+        } catch (ClassNotFoundException e) {
+            message = e.getMessage();
+            JOptionPane.showMessageDialog(null, e);
+        } catch (InstantiationException e) {
+            message = e.getMessage();
+            JOptionPane.showMessageDialog(null, e);
+        } catch (IllegalAccessException e) {
+            message = e.getMessage();
+            JOptionPane.showMessageDialog(null, e);
+        } catch (SQLException e) {
+            message = e.getMessage();
+            JOptionPane.showMessageDialog(null, e);
+        } 
+    }
+    
+    public boolean getConectado(){
+        return this.conectado;
+    }
+    public String getMessage(){
+        return this.message;
+    }
+    
+    public Statement getSt(){
+        return st;
+    }
+    
+    public void CerrarConexion(){
+        try {
+            ct.close();
+        } catch (SQLException ex) { 
+        }
+        
+    }
+    
+    
     
 }
