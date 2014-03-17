c351d818535553c3aac4cc87e2b5ed871dd922d7
CBedoy <carlos.bedoy@gmail.com>
2014-03-16 23:40:55 -0600

    Add class Country and Locations

---
+++ cb/models/Location.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cb.models;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Location {
+    private int id;
+    private String name;
+    private Country country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
