56e5118b3fc49e9ba836e0a2e2fdd28233552e4a
CBedoy <carlos.bedoy@gmail.com>
2014-03-16 23:42:47 -0600

    Change properties of gransslam class

--- cb/models/GrandSlam.java
+++ cb/models/GrandSlam.java
@@ -15,9 +15,8 @@ import cb.interfaces.IModel;
 public class GrandSlam implements IModel{
     private int id;
     private String name;
-    private String country;
-    private String place;
     private String date;
+    private Country country;
 
     public int getId() {
         return id;
@@ -35,28 +34,20 @@ public class GrandSlam implements IModel{
         this.name = name;
     }
 
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getPlace() {
-        return place;
-    }
-
-    public void setPlace(String place) {
-        this.place = place;
-    }
-
     public String getDate() {
         return date;
     }
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
     }
     
     
