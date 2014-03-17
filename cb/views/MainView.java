d30611d85ffb5a9002aca62e4b2168ab1d2af272
CBedoy <carlos.bedoy@gmail.com>
2014-03-17 00:30:24 -0600

    Add menus to view

--- cb/views/MainView.java
+++ cb/views/MainView.java
@@ -32,7 +32,18 @@ public class MainView extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenuItem9 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -52,12 +63,71 @@ public class MainView extends javax.swing.JFrame {
         jMenuBar1.setBackground(new java.awt.Color(238, 239, 243));
 
         jMenu1.setForeground(new java.awt.Color(51, 51, 51));
-        jMenu1.setText("File");
+        jMenu1.setText("New");
+
+        jMenuItem1.setBackground(new java.awt.Color(238, 239, 243));
+        jMenuItem1.setForeground(new java.awt.Color(51, 51, 51));
+        jMenuItem1.setText("Award");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setBackground(new java.awt.Color(238, 239, 243));
+        jMenuItem2.setForeground(new java.awt.Color(51, 51, 51));
+        jMenuItem2.setText("Country");
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setBackground(new java.awt.Color(238, 239, 243));
+        jMenuItem3.setForeground(new java.awt.Color(51, 51, 51));
+        jMenuItem3.setText("Game");
+        jMenu1.add(jMenuItem3);
+
+        jMenuItem4.setBackground(new java.awt.Color(238, 239, 243));
+        jMenuItem4.setForeground(new java.awt.Color(51, 51, 51));
+        jMenuItem4.setText("GrandSlam");
+        jMenu1.add(jMenuItem4);
+
+        jMenuItem5.setBackground(new java.awt.Color(238, 239, 243));
+        jMenuItem5.setForeground(new java.awt.Color(51, 51, 51));
+        jMenuItem5.setText("Location");
+        jMenu1.add(jMenuItem5);
+
+        jMenuItem6.setBackground(new java.awt.Color(238, 239, 243));
+        jMenuItem6.setForeground(new java.awt.Color(51, 51, 51));
+        jMenuItem6.setText("Nacionality");
+        jMenu1.add(jMenuItem6);
+
+        jMenuItem7.setBackground(new java.awt.Color(238, 239, 243));
+        jMenuItem7.setForeground(new java.awt.Color(51, 51, 51));
+        jMenuItem7.setText("Player");
+        jMenu1.add(jMenuItem7);
+
+        jMenuItem8.setBackground(new java.awt.Color(238, 239, 243));
+        jMenuItem8.setForeground(new java.awt.Color(51, 51, 51));
+        jMenuItem8.setText("Referee");
+        jMenu1.add(jMenuItem8);
+
+        jMenuItem9.setBackground(new java.awt.Color(238, 239, 243));
+        jMenuItem9.setForeground(new java.awt.Color(51, 51, 51));
+        jMenuItem9.setText("Trainer");
+        jMenu1.add(jMenuItem9);
+
         jMenuBar1.add(jMenu1);
 
         jMenu2.setForeground(new java.awt.Color(51, 51, 51));
-        jMenu2.setText("Edit");
+        jMenu2.setText("Search");
         jMenuBar1.add(jMenu2);
+
+        jMenu3.setForeground(new java.awt.Color(51, 51, 51));
+        jMenu3.setText("Edit");
+        jMenuBar1.add(jMenu3);
+
+        jMenu4.setForeground(new java.awt.Color(51, 51, 51));
+        jMenu4.setText("Delete");
+        jMenuBar1.add(jMenu4);
 
         setJMenuBar(jMenuBar1);
 
@@ -74,6 +144,10 @@ public class MainView extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -113,7 +187,18 @@ public class MainView extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
+    private javax.swing.JMenuItem jMenuItem9;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
