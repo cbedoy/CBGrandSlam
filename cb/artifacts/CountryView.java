08b49bcc554a20688b4bf3985b10980c5c98bee3
CBedoy <carlos.bedoy@gmail.com>
2014-03-17 22:23:49 -0600

    Add comments autor

--- cb/artifacts/CountryView.java
+++ cb/artifacts/CountryView.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package cb.artifacts;
 
@@ -11,7 +7,15 @@ import cb.interfaces.IArtifact;
 
 /**
  *
- * @author Carlos
+ * @author Carlos Alfredo Cervantes Bedoy
+ * Mobile Developer
+ * CBGrandSlam
+ * Email:           carlos.bedoy@gmail.com
+ * Facebook:        https://www.facebook.com/carlos.bedoy
+ * Github:          https://github.com/cbedoy
+ * WebSite:         http://cbedoy.github.io/
+ *
+ * 17-mar-2014 - 22:22:34
  */
 public class CountryView extends javax.swing.JPanel implements IArtifact {
 
